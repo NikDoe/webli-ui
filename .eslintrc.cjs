d0d03cc3e27cb860ade6a1d8ff192a7dd688a8c5
NikDoe <nikd0e@yandex.ru>
2023-02-06 15:21:05 +0300

    add linters to the project w/ pre-commit hooks

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	overrides: [],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"react/react-in-jsx-scope": "off",
+		"linebreak-style": 0,
+		"no-console": ["error"]
+	},
+};
