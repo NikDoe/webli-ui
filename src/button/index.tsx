71fa5be33390ae219e523c300dcf9dd3730dc739
NikDoe <nikd0e@yandex.ru>
2023-02-07 19:02:35 +0300

    create button UI component

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,12 +1,31 @@
-import { FC, ReactNode } from "react";
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC, MouseEvent } from "react";
 
-interface IButtonProps {
-	color: string;
-	children: ReactNode;
+type ButtonType = "button" | "submit" | "reset";
+
+interface IButtonProps
+	extends DetailedHTMLProps<
+			ButtonHTMLAttributes<HTMLButtonElement>,
+			HTMLButtonElement
+		>,
+		React.AriaAttributes {
+	className?: string;
+	type?: ButtonType;
+	isDisabled?: boolean;
+	onClick: (event: MouseEvent) => void;
 }
 
-const Button: FC<IButtonProps> = ({ children }) => {
-	return <button>{children}</button>;
+const Button: FC<IButtonProps> = ({
+	children,
+	type,
+	isDisabled,
+	onClick,
+	...props
+}) => {
+	return (
+		<button type={type} disabled={isDisabled} onClick={onClick} {...props}>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
