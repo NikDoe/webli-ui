bd303ba7ca4dddf8d3188e1a3af64c214d92e679
NikDoe <nikd0e@yandex.ru>
2023-03-07 13:12:26 +0300

    fix: center icon in password field

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FormField } from "./components";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<FormField type="text" />
-		<FormField type="password" />
-	</React.StrictMode>,
+	<React.StrictMode></React.StrictMode>,
 );
