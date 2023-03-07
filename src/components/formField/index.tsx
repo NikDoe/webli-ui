bfef915dca15eb6f61624d720f5961ce472025f0
NikDoe <nikd0e@yandex.ru>
2023-03-07 13:09:40 +0300

    feat: add password form field

--- src/components/formField/index.tsx
+++ src/components/formField/index.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { IFormField } from "../../types";
+import Icon from "../icon";
 import Input from "../input";
 
 import styles from "./styles.module.scss";
@@ -17,6 +18,44 @@ const FormField: FC<IFormField> = ({
 	onFocus,
 	...props
 }) => {
+	const [isShowPassword, setIsShowPassword] = useState(false);
+
+	const handleShowPassword = () => {
+		setIsShowPassword((prevState) => !prevState);
+	};
+
+	const handleType = () => {
+		if (type !== "text") {
+			return (type = isShowPassword ? "text" : "password");
+		}
+		return "text";
+	};
+
+	const renderInput = () => {
+		return (
+			<>
+				<Input
+					className={clsx(isFocused && styles.Input__active)}
+					name={name}
+					error={error}
+					onBlur={onBlur}
+					onFocus={onFocus}
+					type={handleType()}
+					{...props}
+				/>
+				{((type === "password" && !isShowPassword) ||
+					(type !== "password" && isShowPassword)) && (
+					<Icon
+						type={isShowPassword ? "visibleoff" : "visible"}
+						className={styles.Visible}
+						onClick={handleShowPassword}
+					/>
+				)}
+				{error && <div className={styles.Error}>{error}</div>}
+			</>
+		);
+	};
+
 	return (
 		<div
 			className={clsx(
@@ -31,20 +70,7 @@ const FormField: FC<IFormField> = ({
 					<span className={styles.Label__required}> *</span>
 				)}
 			</label>
-			{type === "text" && (
-				<>
-					<Input
-						className={clsx(isFocused && styles.Input__active)}
-						name={name}
-						error={error}
-						onBlur={onBlur}
-						onFocus={onFocus}
-						type={type}
-						{...props}
-					/>
-					{error && <div className={styles.Error}>{error}</div>}
-				</>
-			)}
+			{renderInput()}
 		</div>
 	);
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { FormField } from "./components";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode></React.StrictMode>,
+	<React.StrictMode>
+		<FormField type="text" />
+		<FormField type="password" />
+	</React.StrictMode>,
 );
